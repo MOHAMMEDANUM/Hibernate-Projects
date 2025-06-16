13efb7066a3b220e731e891aea18f149f357a78d
Mohammed Anum <160215305+MOHAMMEDANUM@users.noreply.github.com>
2025-06-16 10:40:20 +0530

    Removed unnecessary creation of table using @GeneratedValue(strategy = GenerationType.IDENTITY)

--- school/src/main/java/school/Student.java
+++ school/src/main/java/school/Student.java
@@ -2,13 +2,14 @@ package school;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Student {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String email;
